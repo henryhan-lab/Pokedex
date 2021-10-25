ccfcb95a82aca08354035d01d61ef9e7d58277f5
Rui Han <henryhan0415@gmail.com>
2021-10-24 23:14:39 -0700

    updated pokedex component list

--- src/pokedex.js
+++ src/pokedex.js
@@ -2,15 +2,8 @@
 import React, { Component,useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import GifPlayer from 'react-gif-player'
-
-
-
-const GET_POKEMON_NAMES = gql`
-
-query getName{
-    getAllPokemonSpecies
-}
-`
+import Box from '@mui/material/Box';
+import { alpha } from '@mui/material/styles';
 
 
 const GET_POKEMON_INFO = gql`
@@ -47,13 +40,100 @@ function PokedexItem (props){
       );
     if (loading) return null;
     if (error) return `Error! ${error}`;  
-    return(<img  alt = "" src = {data.getPokemonByDexNumber.sprite} />)
+    return(
+	<div align ="center">
+	    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
+        alignItems: 'flex-end	',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+		width:350,
+		margin:10
+      }}
+    >
+
+	  <Box sx={{
+
+		  width:400,
+		  alignSelf:'center',
+		  m:'10',
+	  }}>
+
+      <Box
+        component="img"
+		
+        sx={{
+			m:'10',
+			alignSelf: 'center'
+        }}
+        alt="The house from the offer."
+        src={data.getPokemonByDexNumber.sprite}
+      />
+		</Box>
+
+
+
+		<Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: { xs: 'center', md: 'flex-start		' },
+          m: 3,
+		  alignSelf:'flex-start'
+		  
+          
+        }}
+      >
+        <Box component="span" sx={{ fontSize: 16, mt: 1 }}>
+		{data.getPokemonByDexNumber.species}
+        </Box>
+
+        <Box component="inline" sx={{ alignContent: "flex-start", color: 'primary.main', fontSize: 12 }}>
+		{data.getPokemonByDexNumber.flavorTexts[0].flavor}
+        </Box>
+
+		<Box component="span" sx={{ alignSelf: "flex-start	 ", color: 'primary.main', fontSize: 12 }}>
+		Attack: {data.getPokemonByDexNumber.baseStats.attack}
+        </Box>
+
+		<Box component="span" sx={{ color: 'primary.main', fontSize: 12 }}>
+		Defense: {data.getPokemonByDexNumber.baseStats.defense}
+        </Box>
+
+		<Box component="span" sx={{ color: 'primary.main', fontSize: 12 }}>
+		Special Attack: {data.getPokemonByDexNumber.baseStats.specialattack}
+        </Box>
+
+		<Box component="span" sx={{ color: 'primary.main', fontSize: 12 }}>
+		Special Defense: {data.getPokemonByDexNumber.baseStats.specialdefense}
+        </Box>
+
+
+
+		</Box>
+	
+
+	
+	</Box>
+
+	</div>
+	)
 }
 
 
 const Pokedex = (props) => {
+
+
+
+
+
     var rows = [];
-    for (var i = 1; i < 4; i++) {
+    for (var i = 1; i < 10; i++) {
         rows.push(<PokedexItem id = {i} />);
     }
     return(
